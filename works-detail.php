54aa9dfcfb58b0107cb6655a7b486c0475de5833
binojcode <Binoj@Binoj.local>
2016-09-09 19:53:08 +0530

    work detail

---
+++ works-detail.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>A10 Digital Branding</title>
+
+
+
+<!-- CSS Files -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/ionicons.min.css" rel="stylesheet">
+<link href="css/outline.css" rel="stylesheet">
+<link href="css/jquery.bxslider.css" rel="stylesheet">
+<link href="css/animate.css" rel="stylesheet">
+<link href="css/owl.carousel.css" rel="stylesheet">
+<link href="css/odometer.css" rel="stylesheet" type="text/css" media="all"/>
+<link href="css/base.css" rel="stylesheet"/>
+<link href="css/jquery.fancybox.css" rel="stylesheet"/>
+<link href="css/outline.css" rel="stylesheet" type="text/css">
+<link href="css/component.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet">
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+<?php
+include('extra_head.php');
+?>
+
+
+</head>
+<body>
+<div class="loader"> <svg class="spinner" id="pageloader_anime" width="32px" height="32px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+  <circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
+  </svg> </div>
+
+
+<?php
+include('menu.php')
+
+?>
+
+<header class="inner">
+ 	<div class="content  overlay">
+    		<div class="centered">
+          <h1>Works</h1>
+          <h5>WE MAKE PROJECT WITH PASSION</h5>
+          </div>
+           <!-- end centered -->
+          </div>
+          <!-- end content -->
+  <div class="wrap">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+           <nav class="navbar navbar-default  navbar-header" role="navigation">
+            <a href="index.php">
+          <img src="images/home/a10-white.png" width="150" alt="Image" class="logo" /></a>
+            
+            
+            
+            
+            <button type="button" class="navbar-toggle toggle-menu menu-right push-body" data-toggle="collapse"> <i class="ion-navicon"></i> </button>
+            
+            <div class="col-md-6 hidden-sm hidden-xs" style="float: right; margin-top: 30px;">
+                  <ul class="social-media">
+                    <li><a target="_blank" href="http://www.facebook.com/a10digital"><i class="ion-social-facebook"></i></a></li>
+                    <li><a target="_blank" href="http://twitter.com/a10digital"><i class="ion-social-twitter"></i></a></li>
+                    <li><a target="_blank" href="http://www.instagram.com/a10digital"><i class="ion-social-instagram"></i></a></li>
+                     <li><a target="_blank" href="https://www.linkedin.com/company/a10-digital-marketing-solutions"><i class="ion-social-linkedin"></i></a></li>
+                     <li><a target="_blank" href="https://www.youtube.com/channel/UCC40BlEodDyY1KjPUdBRPuQ"><i class="ion-social-youtube"></i></a></li>
+                     <li><a target="_blank" href="https://plus.google.com/u/0/102489498587900067976"><i class="ion-icon-social-google-plus"></i></a></li>
+                  </ul>
+            </div>
+         
+          </nav>
+          <!-- end nav -->
+        </div>
+        <!-- end col-12 --> 
+      </div>
+      <!-- end row --> 
+    </div>
+    <!-- end container --> 
+  </div>
+  <!-- end wrap --> 
+</header>
+<!-- end header -->
+<section class="works-detail section-space">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 col-md-offset-2 text-center">
+        <h2 class="main-title">Crealive digital</h2>
+        <h3 class="sub-title">We designed & developed a revolutionary agency site for Crealive with “search” at the core of the user experience.</h3>
+      </div>
+      <!-- end col-8 -->
+      <div class="col-md-8 col-md-offset-2 text-justify">
+        <ul class="works-detail-carousel">
+          <li><img src="images/works-detail1.jpg" alt="Image"></li>
+          <li><img src="images/works-detail2.jpg" alt="Image"></li>
+          <li><img src="images/works-detail3.jpg" alt="Image"></li>
+        </ul>
+        <!-- end ul -->
+        <h3>Desription ;</h3>
+        <p>Based on Google Search Appliance the engine dynamically creates relevant content as quick & smart results, such as a pinned map when searching for a branch or current rates when searching for a foreign currency. </p>
+        <img src="images/imac-detail.jpg" alt="Image">
+        <h3>Details ;</h3>
+        <small>Client / YKB</small><br>
+        <small>Tags / Banking, BehaviorPad, Finance, Google Search Appliance</small><br>
+        <small>URL / www.crealive.com</small> </div>
+      <!-- end col-8 --> 
+    </div>
+    <!-- end row --> 
+  </div>
+  <!-- end container --> 
+</section>
+<!-- end section -->
+<!-- footer -->
+<?php
+include('footer.php');
+?>
+<!-- footer ends-->
+
+
+<!-- JS Files --> 
+<script src="js/jquery.min.js"></script> 
+<script type="text/javascript">
+$(window).load(function() {
+	$(".loader").fadeOut("fast");
+})
+</script>
+<?php
+include('js.php');
+?>
+
+
+</body>
+
+
+</html>
